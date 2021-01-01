04db32165d8fd1a42b59b361695c0d60a16fbf7f
surendhraganji <surendra.gan@gmail.com>
2021-01-01 01:34:32 -0500

    git init

---
+++ pyspark_logging.py
@@ -0,0 +1,21 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+import logging
+logging.basicConfig(filename='/Users/surendhra.ganji1@ibm.com/Documents/surendhra/Spark/logs/python.log',level=logging.ERROR,filemode='w')
+def main():
+
+ try:
+
+  spark1 = SparkConf().setMaster("local[*]").setAppName("rdd logging")
+  sc = SparkContext(conf=spark1)
+  sc._jvm.org.apache.log4j.Logger
+  file1=sc.textFile("/Users/surendhra.ganji1@ibm.com/Documents/surendhra/Spark/rawfiles/sampledat.txt").flatMap(lambda f: f.split(" "))\
+    .map(lambda x: (x,1)).reduceByKey(lambda x,y: (x+y))
+  print(file1.collect())
+  except Exception as e:
+  logging.error("error in spark code")
+
+ finally: sc.stop()
+
+if __name__ == '__main__':
+    main()
